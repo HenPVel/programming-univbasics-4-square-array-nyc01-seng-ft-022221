9669334609bb648f65ca2c11fec742d346c8c324
Henry Velasquez <hvelas93@gmail.com>
2021-02-10 03:38:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,12 @@
 array=[1,2,3]
 
+require 'pry'
 def square_array(array)
   count=0
 while count < array.length do
   sq_array=array[count]**2
   count+=1
+  binding.pry
 end
 return square_array
 end
